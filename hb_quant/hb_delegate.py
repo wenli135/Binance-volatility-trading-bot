937eb930da4604e40afd76502806f6324162580b
WenLi <wenli@quchaogu.com>
2021-06-14 10:50:14 +0800

    dev

---
+++ hb_quant/hb_delegate.py
@@ -0,0 +1,83 @@
+#coding: utf8
+from huobi.client.generci import GenericClient
+from huobi.client.trade import TradeClient
+from huobi.constant.definition import OrderType
+
+class General(object):
+    '''
+        不设计交易，不需要账户信息
+    '''
+
+    def __init__(self, symbols, url="https://api.huobi.pro"):
+        self._symbol_list = symbols
+        self._market = MarketClient(url = url)
+        self.symbolInfo = {}
+        self._getSymboInfo()
+
+    def _getSymbolInfo(self):
+    '''
+        获取关心的交易对信息，如报价精度等
+    '''
+        gc = GenericClient()
+        lst = gc.get_exchange_symbols()
+        for sym in lst:
+            if sym.symbol not in self._symbol_list:
+                continue
+            tmp = {}
+            tmp['price_precision'] = sym.price_precision
+            tmp['amount_precision'] = sym.amount_precision
+            tmp['value_precision'] = sym.value_precision
+            tmp['symbol'] = sym.symbol
+            self.symbolInfo[sym.symbol] = tmp
+
+    def getTickers(self):
+    '''
+        获取关心的交易对的最新价格
+    '''
+        ret = []
+        lst = self._market.get_market_tickers()
+        for mt in lst:
+            if mt.symbol not in self._symbol_list:
+                continue
+            tmp = {}
+            tmp['symbol'] = mt.symbol
+            tmp['price'] = mt.close
+            ret[mt.symbol] = tmp
+        return ret
+
+
+class Trade(object):
+
+    def __init__(self, accountId, apiKey, secretKey, url):
+        self._account_id = accountId
+        self._trade = TradeClient(api_key=apiKey, secret_key=secretKey,url=url)
+
+    def marketBuy(self, symbol, amount, clientOrderId = None):
+        orderId = self._trade.create_spot_order(symbol, self._account_id, OrderType.BUY_MARKET, amount, 0.0, client_order_id = clientOrderId)
+        return orderId
+
+    def limitBuy(self, symbol, amount, price, clientOrderId = None):
+        orderId = self._trade.create_spot_order(symbol, self._account_id, OrderType.BUY_LIMIT, amount, price, client_order_id = clientOrderId)
+        return orderId
+
+    def marketSell(self, symbol, amount, clientOrderId = None):
+        orderId = self._trade.create_spot_order(symbol, self._account_id, OrderType.SELL_MARKET, amount, 0.0, client_order_id = clientOrderId)
+        return orderId
+
+    def limitSell(self, symbol, amount, price, clientOrderId = None):
+        orderId = self._trade.create_spot_order(symbol, self._account_id, OrderType.SELL_LIMIT, amount, price, client_order_id = clientOrderId)
+        return orderId
+
+    def getOrderStatus(self, orderId):
+        info = self._trade.get_match_results_by_order_id(orderId)
+        
+            
+if __name__ == '__main__':
+    symbols = ['btcusdt','ethusdt']
+    gTest = General(symbols)            
+    print("{}".format(gTest.symboInfo))
+    
+        
+        
+
+
